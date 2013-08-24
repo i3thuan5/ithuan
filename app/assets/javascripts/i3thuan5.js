4d2ffd969f66cf150c5d4dc1b60cbdc624b80bf5
Ihc <Ihc@gmail.com>
2013-08-24 12:58:13 +0800

    若無是火狐抑是孔，愛警告　丞宏

--- app/assets/javascripts/i3thuan5.js
+++ app/assets/javascripts/i3thuan5.js
@@ -1,4 +1,5 @@
-function pian3tit8() {
+function pian3tit8()
+{
 	// <![CDATA[
 	var taketori = (new Taketori()).set({
 		lang : 'zh-tw',
@@ -9,12 +10,32 @@ function pian3tit8() {
 	// ]]>
 }
 
-function bang2tsam7khai1si2() {
+function bang2tsam7khai1si2()
+{
 	liu5lam2khi3 = BrowserDetect.browser;
 	tsoo2han3ji7();
 	if (liu5lam2khi3 == 'Firefox')
 		pian3tit8();
-	/*
-	 * else if(liu5lam2khi3=='Chrome') pian3tit8();
-	 */
+
+	else if (liu5lam2khi3 == 'Chrome')
+	{
+	} else
+	{
+		瀏覽器警告();
+	}
+
+}
+
+function 瀏覽器警告()
+{
+	if ($.cookie("看過警告") == null)
+	{
+		alert("您知道您的瀏覽器不支援新技術嗎！？會嚴重影響本站品質，請使用Firefox或Chrome！");
+	}
+	var date = new Date();
+	var minutes = 30;
+	date.setTime(date.getTime() + (minutes * 60 * 1000));
+	$.cookie("看過警告", "有", {
+		expires : date
+	});
 }
