13927e9992f643c4d88805d3142e20589f77059c
薛丞宏 <ihcaoe@gmail.com>
2014-09-11 00:15:11 +0800

    加翻譯而且模組化 丞宏

--- 服務/網址.py
+++ 服務/網址.py
@@ -2,8 +2,10 @@
 from django.conf.urls import patterns, url
 from 意傳網站.介面 import 關於意傳
 from 服務.介面 import 閩南語標音
+from 服務.介面 import 閩南語翻譯
 
 urlpatterns = patterns('',
 	url(r'^自動標音/(?P<腔口>[^/]+)/(?P<語句>[^/]*)$', 閩南語標音, name='閩南語標音'),
+	url(r'^華語翻譯/(?P<腔口>[^/]+)/(?P<語句>[^/]*)$', 閩南語翻譯, name='閩南語翻譯'),
 	url(r'^.*$', 關於意傳.as_view(), name='關於意傳'),
 )
