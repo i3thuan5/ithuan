9266d44737af808a56fb20d10c8004c0cd946660
薛丞宏 <ihcaoe@gmail.com>
2014-09-11 01:01:09 +0800

    加標音，猶未好 丞宏

--- 服務/介面.py
+++ 服務/介面.py
@@ -1,37 +1,36 @@
-from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
-from 臺灣言語工具.解析整理.文章粗胚 import 文章粗胚
-from 分言語.語言判斷 import 判斷
 from 臺灣言語工具.綜合標音.句綜合標音 import 句綜合標音
 from 臺灣言語工具.綜合標音.閩南語字綜合標音 import 閩南語字綜合標音
 from django.http.response import HttpResponse
 import json
-from 臺灣言語工具.翻譯.斷詞斷字翻譯 import 斷詞斷字翻譯
-from 臺灣言語工具.翻譯.摩西工具.摩西用戶端 import 摩西用戶端
-from 臺灣言語工具.音標系統.官話.官話注音符號 import 官話注音符號
-from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from 臺灣言語工具.翻譯.摩西工具.語句編碼器 import 語句編碼器
-from 臺灣言語工具.斷詞.中研院工具.官方斷詞剖析工具 import 官方斷詞剖析工具
 from 服務.標音翻譯 import 閩南語翻譯物件
 from 服務.標音翻譯 import 閩南語標音物件
+from 服務.標音翻譯 import 章物件轉標仔
+from 臺灣言語工具.語音合成.句物件轉合成標仔 import 句物件轉合成標仔
 
-_粗胚 = 文章粗胚()
-_分析器=拆文分析器()
-_判斷 = 判斷
+_句物件轉合成標仔=句物件轉合成標仔()
 
-_斷詞斷字翻譯=斷詞斷字翻譯()
-_官方斷詞剖析工具=官方斷詞剖析工具()
-_編碼器 = 語句編碼器()
-斷詞用戶端=摩西用戶端('localhost',8105,編碼器=_編碼器)
-斷字用戶端=摩西用戶端('localhost',8205,編碼器=_編碼器)
 
-def 閩南語翻譯(request,腔口,語句):
-	閩南語章物件=閩南語翻譯物件(腔口, 語句)
-	結果=句綜合標音(閩南語字綜合標音,閩南語章物件)
-	print(結果.轉json格式())
-	return HttpResponse(json.dumps(結果.轉json格式()), content_type="application/json")
-
-def 閩南語標音(request,腔口,語句):
+def 閩南語標音(request, 腔口, 語句):
 	斷詞物件 = 閩南語標音物件(腔口, 語句)
-	結果=句綜合標音(閩南語字綜合標音,斷詞物件)
-	print(結果.轉json格式())
+	結果 = 句綜合標音(閩南語字綜合標音, 斷詞物件)
+# 	print(結果.轉json格式())
 	return HttpResponse(json.dumps(結果.轉json格式()), content_type="application/json")
+
+def 閩南語翻譯(request, 腔口, 語句):
+	閩南語章物件 = 閩南語翻譯物件(腔口, 語句)
+	結果 = 句綜合標音(閩南語字綜合標音, 閩南語章物件)
+# 	print(結果.轉json格式())
+	return HttpResponse(json.dumps(結果.轉json格式()), content_type="application/json")
+
+def 閩南語語音合成(request, 腔口, 語句):
+	斷詞物件 = 閩南語標音物件(腔口, 語句)
+	標仔陣列=_句物件轉合成標仔.句物件轉標仔(斷詞物件)
+	
+	結果 = 句綜合標音(閩南語字綜合標音, 斷詞物件)
+	print(標仔陣列,',斷詞物件')
+	return 章物件轉標仔(標仔陣列)
+
+def 閩南語翻譯合成(request, 腔口, 語句):
+	閩南語章物件 = 閩南語翻譯物件(腔口, 語句)
+	標仔陣列=_句物件轉合成標仔.章物件轉標仔(閩南語章物件)
+	return 章物件轉標仔(標仔陣列)
--- 服務/網址.py
+++ 服務/網址.py
@@ -3,9 +3,13 @@ from django.conf.urls import patterns, url
 from 意傳網站.介面 import 關於意傳
 from 服務.介面 import 閩南語標音
 from 服務.介面 import 閩南語翻譯
+from 服務.介面 import 閩南語語音合成
+from 服務.介面 import 閩南語翻譯合成
 
 urlpatterns = patterns('',
 	url(r'^自動標音/(?P<腔口>[^/]+)/(?P<語句>[^/]*)$', 閩南語標音, name='閩南語標音'),
 	url(r'^華語翻譯/(?P<腔口>[^/]+)/(?P<語句>[^/]*)$', 閩南語翻譯, name='閩南語翻譯'),
+	url(r'^語音合成/(?P<腔口>[^/]+)/(?P<語句>[^/]*).wav$', 閩南語語音合成, name='閩南語語音合成'),
+	url(r'^翻譯合成/(?P<腔口>[^/]+)/(?P<語句>[^/]*).wav$', 閩南語翻譯合成, name='閩南語翻譯合成'),
 	url(r'^.*$', 關於意傳.as_view(), name='關於意傳'),
 )
