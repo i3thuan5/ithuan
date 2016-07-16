4399098919e0f6c426a44c7eb9a18dab00349430
薛丞宏 <ihcaoe@gmail.com>
2016-07-16 10:23:14 +0800

    網址有問題就跳到首頁

--- src/網址.jsx
+++ src/網址.jsx
@@ -22,6 +22,7 @@ render(
               <Route path='%E7%B7%9A%E4%B8%8A%E7%B5%84%E5%AD%97' component={線上組字}/>
               <Route path='%E9%96%A9%E5%AE%A2%E6%95%85%E4%BA%8B' component={閩客故事}/>
               <Route path='%E7%9B%B8%E9%97%9C%E9%80%A3%E7%B5%90' component={相關連結}/>
+              <Route path='*' component={首頁}/>
           </Route>
   </Router>,
   root
