3c0057436cefbcafec93ab2dc71e6737b62569aa
a8568730 <a8568730@hotmail.com>
2017-01-12 14:09:28 +0800

    我們的服務

--- src/網站/網站.jsx
+++ src/網站/網站.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-// import './網站.css';
-import 目錄 from './目錄';
+// import 目錄 from './目錄';
 import 'semantic-ui-css/semantic.min.css';
 
 export default class 網站 extends React.Component {
 
   render () {
-    return (
-      <div id="網頁最外圍">
-        <div id="leftWrapper">
-            {this.props.children}
-        </div>
-      </div>
-    );
+    return this.props.children;
   }
 }
--- src/頁/首頁/首頁.jsx
+++ src/頁/首頁/首頁.jsx
@@ -5,11 +5,12 @@ import { Link } from 'react-router';
 // import 翻譯 from './翻譯.png';
 // import 發音 from './發音.png';
 // import 組字 from './組字.png';
-import { Container, Segment } from 'semantic-ui-react';
+import { Container, Segment, Header, Grid, Item, Step, Icon } from 'semantic-ui-react';
 export default class 首頁 extends React.Component {
 
   render () {
     return (
+      <div>
       <Segment textAlign='center' inverted vertical>
 
     <Container>
@@ -17,22 +18,81 @@ export default class 首頁 extends React.Component {
         <a className="toc item">
           <i className="sidebar icon"></i>
         </a>
-        <a className="active item">Home</a>
+        <Link to="/" className="active item">首頁</Link>
         <a className="item">Work</a>
         <a className="item">Company</a>
         <a className="item">Careers</a>
       </div>
     </Container>
 
-    <Container className="ui text container">
-      <h1 className="ui inverted header">
-        意傳文化科技
-      </h1>
-      <h2>Do whatever you want when you want to.</h2>
-      <div className="ui huge primary button">Get Started <i className="right arrow icon"></i></div>
+    <Container text>
+      <Header as='h1' inverted>意傳文化科技</Header>
+      <Header as='h2' inverted>Do whatever you want when you want to.</Header>
+      <div className="ui huge primary button">了解意傳 <i className="right arrow icon"></i></div>
     </Container>
-
   </Segment>
+
+    <Segment vertical>
+        <Header as='h2' textAlign='center'>我們的服務</Header>
+        <Grid stackable columns={2}>
+          <Grid.Row>
+            <Grid.Column>
+            
+              <Step.Group>
+                <Step>
+                  <Icon name='truck' />
+                  <Step.Content>
+                    <Step.Title>語音辨識</Step.Title>
+                    <Step.Description>Choose your shipping options</Step.Description>
+                  </Step.Content>
+                </Step>
+              </Step.Group>
+            </Grid.Column>
+
+             <Grid.Column>
+              <Step.Group>
+                <Step>
+                  <Icon name='truck' />
+                  <Step.Content>
+                    <Step.Title>語音合成</Step.Title>
+                    <Step.Description>Choose your shipping options</Step.Description>
+                  </Step.Content>
+                </Step>
+              </Step.Group>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+        <Grid stackable columns={2}>
+          <Grid.Row>
+            <Grid.Column>
+              <Step.Group>
+                <Step>
+                  <Icon name='database' />
+                  <Step.Content>
+                    <Step.Title>整理語料</Step.Title>
+                    <Step.Description>Choose your shipping options</Step.Description>
+                  </Step.Content>
+                </Step>
+              </Step.Group>
+            </Grid.Column>
+
+             <Grid.Column>
+              <Step.Group>
+                <Step>
+                  <Icon name='text cursor' />
+                  <Step.Content>
+                    <Step.Title>對齊漢字音標</Step.Title>
+                    <Step.Description>Choose your shipping options</Step.Description>
+                  </Step.Content>
+                </Step>
+              </Step.Group>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+    </div>
       );
   }
 }
