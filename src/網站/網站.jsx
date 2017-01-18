39a77f0186b6c4b728173bee4a4a1601b11ce7a8
a8568730 <a8568730@hotmail.com>
2017-01-18 11:02:11 +0800

    標示目前左側頁面項目

--- src/網站/網站.jsx
+++ src/網站/網站.jsx
@@ -12,16 +12,37 @@ var debug = Debug('ithuan:網站');
 export default class 網站 extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { visible: false };
+    this.state = {
+      visible: false,
+      activeItem: this.props.location.pathname || '/',
+      paths: {
+        home: '/',
+        tsooji: '/%E7%B7%9A%E4%B8%8A%E7%B5%84%E5%AD%97',
+        kosu: '/%E9%96%A9%E5%AE%A2%E6%95%85%E4%BA%8B',
+        siongkuan: '/%E7%9B%B8%E9%97%9C%E9%80%A3%E7%B5%90',
+      },
+    };
   }
 
   toggleVisibility() {
     this.setState({ visible: !this.state.visible });
+  }
 
+  handleItemClick(activeItem) {
+    this.setState({ activeItem });
+  }
+
+  getClasses(link) {
+    if (this.state.activeItem === link) {
+      return 'active item';
+    }
+
+    return 'item';
   }
 
   render () {
-    const { visible } = this.state;
+    const { visible, activeItem, paths } = this.state;
+
     return (
       <div>
        <Menu id="KuahPau" icon vertical inverted fixed='top'>
@@ -34,26 +55,18 @@ export default class 網站 extends React.Component {
             </Menu>
         <Sidebar.Pushable>
           <Sidebar as={Menu} fixed="left" animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
-            <Menu.Item name='home'>
-              <Link to="/" className="active item">
-                <Icon name='home' />首頁
+              <Link to="/" className={this.getClasses(paths.home)} onClick={this.handleItemClick.bind(this, paths.home)}>
+                <Icon name='home'/>首頁
               </Link>
-            </Menu.Item>
-            <Menu.Item name='gamepad'>
-              <Link to="/%E7%B7%9A%E4%B8%8A%E7%B5%84%E5%AD%97" className="item">
-                <Icon name='gamepad' />線上組字
+              <Link to={paths.tsooji} className={this.getClasses(paths.tsooji)} onClick={this.handleItemClick.bind(this, paths.tsooji)}>
+                <Icon name='gamepad'/>線上組字
               </Link>
-            </Menu.Item>
-            <Menu.Item name='camera'>
-              <Link to="/%E9%96%A9%E5%AE%A2%E6%95%85%E4%BA%8B" className="item">
-                <Icon name='camera' />閩客故事
+              <Link to={paths.kosu} className={this.getClasses(paths.kosu)} onClick={this.handleItemClick.bind(this, paths.kosu)}>
+                <Icon name='camera'/>閩客故事
               </Link>
-            </Menu.Item>
-            <Menu.Item name='camera'>
-              <Link to="/%E7%9B%B8%E9%97%9C%E9%80%A3%E7%B5%90" className="item">
-                <Icon name='camera' />相關連結
+              <Link to={paths.siongkuan} className={this.getClasses(paths.siongkuan)} onClick={this.handleItemClick.bind(this, paths.siongkuan)}>
+                <Icon name='camera'/>相關連結
               </Link>
-            </Menu.Item>
           </Sidebar>
           
           <Sidebar.Pusher style={{ height: '100vh', overflowY: 'scroll', overflowX: 'hidden' }}>
--- src/網站/頁首.jsx
+++ src/網站/頁首.jsx
@@ -12,10 +12,10 @@ class 頁首 extends Component {
       <Segment id="appNav" textAlign='center' inverted vertical>
         <Container>
           <Menu secondary inverted size="large" pointing>
-          <Menu.Item><Link to="/" className="active item">首頁</Link></Menu.Item>
-          <Menu.Item><Link to="/%E7%B7%9A%E4%B8%8A%E7%B5%84%E5%AD%97" className="item">線上組字</Link></Menu.Item>
-          <Menu.Item><Link to="/%E9%96%A9%E5%AE%A2%E6%95%85%E4%BA%8B" className="item">閩客故事</Link></Menu.Item>
-          <Menu.Item><Link to="/%E7%9B%B8%E9%97%9C%E9%80%A3%E7%B5%90" className="item">相關連結</Link></Menu.Item>
+            <Link to="/" className="active item">首頁</Link>
+            <Link to="/%E7%B7%9A%E4%B8%8A%E7%B5%84%E5%AD%97" className="item">線上組字</Link>
+            <Link to="/%E9%96%A9%E5%AE%A2%E6%95%85%E4%BA%8B" className="item">閩客故事</Link>
+            <Link to="/%E7%9B%B8%E9%97%9C%E9%80%A3%E7%B5%90" className="item">相關連結</Link>
           </Menu>
         </Container>
       </Segment>
