83ac48d45649c12d452d210c42fb9b5da424b15c
薛丞宏 <ihcaoe@gmail.com>
2018-02-03 21:55:33 +0800

    佮33做伙改理念的文案

--- src/頁/首頁/理念.jsx
+++ src/頁/首頁/理念.jsx
@@ -7,8 +7,8 @@ class 理念 extends Component {
       <Segment basic className="stripe slogan">
         <Container text>
         <Header as='h2' textAlign='center'>逆風高飛吧！</Header>
-        <p>開發台語/閩南語、客語、族語等開源工具，讓學習母語以及研究母語的人可以事半功倍！</p>
-        <p>市面上雖然很多華語工具，但不一定適用全部的語言。我們希望為母語使用者提供另一種選擇。</p>
+        <p>市面上雖然很多華語工具，但不一定適用我們的語言，我們想藉由開發台語、客語和族語各式各樣的開源工具，提供另一種選擇。</p>
+        <p>讓研究母語的人可以事半功倍，學習母語也可以很「時行」！</p>
         </Container>
       </Segment>
     );
