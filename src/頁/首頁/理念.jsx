8f74610cee83e4cfb168b5766332d39f90717e68
薛丞宏 <ihcaoe@gmail.com>
2018-02-03 21:57:48 +0800

    閣順一擺

--- src/頁/首頁/理念.jsx
+++ src/頁/首頁/理念.jsx
@@ -7,8 +7,8 @@ class 理念 extends Component {
       <Segment basic className="stripe slogan">
         <Container text>
         <Header as='h2' textAlign='center'>逆風高飛吧！</Header>
-        <p>市面上雖然很多華語工具，但不一定適用我們的語言，我們想藉由開發台語、客語和族語各式各樣的開源工具，提供另一種選擇。</p>
-        <p>讓研究母語的人可以事半功倍，學習母語也可以很「時行」！</p>
+        <p>市面上雖然很多華語工具，但不一定適用我們的語言，因此我們想藉由各式各樣的台語、客語和族語工具，提供另一種選擇。</p>
+        <p>讓研究母語能夠事半功倍，學習母語也可以很「時行」！</p>
         </Container>
       </Segment>
     );
