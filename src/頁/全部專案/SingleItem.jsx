bdf2fbbb3e75f4be30327c45e0b4df02ca99ea23
a8568730 <a8568730@hotmail.com>
2019-01-31 15:26:27 +0800

    * 全部專案改用list render

--- src/頁/全部專案/SingleItem.jsx
+++ src/頁/全部專案/SingleItem.jsx
@@ -3,9 +3,9 @@ import { Item } from 'semantic-ui-react';
 
 let LogoI3Thuan5='https://grants.g0v.tw/uploads/5d147554034c77e3e66bc0706473714e.png';
 
-const SingleItem = ({logo=LogoI3Thuan5, url, title, meta, children}) => (
+const SingleItem = ({logo=LogoI3Thuan5, size='medium', url, title, meta, children}) => (
   <Item>
-    <Item.Image size='tiny' src={logo} />
+    <Item.Image size={size} src={logo} />
     <Item.Content>
       <Item.Header as='a' href={url} target="_blank">{title}</Item.Header>
       <Item.Meta>{meta}</Item.Meta>
