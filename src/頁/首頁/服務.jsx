635b3cb90ae5d8a02855db6216d016917d56f0e7
naphing <khu1981@gmail.com>
2019-09-18 15:17:36 +0800

    hok-bu kai-pan

--- src/頁/首頁/服務.jsx
+++ src/頁/首頁/服務.jsx
@@ -11,35 +11,26 @@ class 服務 extends Component {
     return (
     <Segment basic className="stripe">
       <Header as='h2' textAlign='center'>阮的服務</Header>
-
-        <Grid container centered stackable>
-
-          <Grid.Column width={5} textAlign="center">
-            <Segment>影音字幕自動化</Segment>
-          </Grid.Column>
-          <Grid.Column width={5} textAlign="center">
-            <Segment>文本發音參考</Segment>
-          </Grid.Column>
-          <Grid.Column width={5} textAlign="center">
-            <Segment>語料標注系統</Segment>
-          </Grid.Column>
-          <Grid.Column width={5} textAlign="center">
-            <Segment>詞頻統計系統</Segment>
-          </Grid.Column>
-          <Grid.Column width={5} textAlign="center">
-            <Segment>客製化語料庫</Segment>
-          </Grid.Column>
-          <Grid.Column width={5} textAlign="center">
-            <Segment>台華雙語對譯</Segment>
-          </Grid.Column>
-          <Grid.Column width={5} textAlign="center">
-            <Segment>AI語音合成</Segment>
-          </Grid.Column>
-          <Grid.Column width={5} textAlign="center">
-            <Segment>AI語音辨識</Segment>
-          </Grid.Column>
-        </Grid>
-
+     <div className="ui segment">
+  <div className="ui two column very relaxed grid">
+    <div className="column">
+      <h4 className="ui right aligned header">
+      <p>影音字幕自動化</p>
+      <p>文本發音參考</p>
+      <p>語料標注系統</p>
+      <p>詞頻統計系統</p></h4>
+    </div>
+    <div className="column">
+    <h4 className="ui left aligned header">
+      <p>客製化語料庫</p>
+      <p>台華雙語對譯</p>
+      <p>AI語音合成</p>
+      <p>AI語音辨識</p></h4>
+    </div>
+  </div>
+  <div className="ui vertical divider"><i className="handshake outline icon"></i></div>
+</div>
+  <br></br>
     </Segment>
     );
   }
