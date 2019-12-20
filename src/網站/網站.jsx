05caa6fdfcc6e79e50c9f4f16c487243eff6aca2
fafoy <ihcaoe@gmail.com>
2019-12-20 20:19:20 +0800

    Tshiu-ki-a pan-bin thiu--tshut-lai

--- src/網站/網站.jsx
+++ src/網站/網站.jsx
@@ -4,6 +4,7 @@ import { Sidebar, Dimmer, Button, Menu, Icon } from 'semantic-ui-react';
 import Debug from 'debug';
 import 頁首 from './頁首';
 import 頁腳 from './頁腳';
+import 手機仔 from './手機仔'
 import 'semantic-ui-css/semantic.min.css';
 import './網站.css';
 
@@ -63,23 +64,10 @@ export default class 網站 extends React.Component {
             </Menu.Item>
             </Menu>
         <Sidebar.Pushable>
-          <Sidebar as={Menu} fixed="left" animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
-              <Link to={paths.home} className={this.getClasses(paths.home)} onClick={this.closeVisibility}>
-                <Icon name='home'/>首頁
-              </Link>
-              <Link to={paths.tsuanan} className={this.getClasses(paths.tsuanan)} onClick={this.closeVisibility}>
-                <Icon name='rocket'/>專案開發
-              </Link>
-              <Link to={paths.suisiann} className={this.getClasses(paths.suisiann)} onClick={this.closeVisibility}>
-                <Icon name='volume up'/>SuíSiann
-              </Link>
-              <Link to={paths.siongkuan} className={this.getClasses(paths.siongkuan)} onClick={this.closeVisibility}>
-                <Icon name='world'/>資源連結
-              </Link>
-              <Link to={paths.lianlok} className={this.getClasses(paths.lianlok)} onClick={this.closeVisibility}>
-                <Icon name='envelope'/>連絡交流
-              </Link>
-          </Sidebar>
+          <手機仔
+            paths={this.state.paths} getClasses={this.getClasses.bind(this)}
+            visible={visible}
+            />
           
           <Sidebar.Pusher id="i3pusher">
             <Dimmer.Dimmable dimmed={visible}>
