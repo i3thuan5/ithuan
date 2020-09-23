d95c39e43fc7ca030a6c55227bd229341c38fb68
a8568730 <a8568730@hotmail.com>
2020-09-23 14:55:33 +0800

    * Chhiú-ki-á-téng ka Phiau-thâu.

--- src/頁/首頁/大看板.jsx
+++ src/頁/首頁/大看板.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Header, Segment } from 'semantic-ui-react';
+import { Container, Header, Image, Segment } from 'semantic-ui-react';
+import favicon from '../../../favicons/android-chrome-192x192.png';
 
 export default class 大看板 extends Component {
   constructor(props) {
@@ -8,11 +9,11 @@ export default class 大看板 extends Component {
 
   render() {
     return (
-      <Segment textAlign='center' inverted vertical>
+      <Segment textAlign='center' inverted vertical className='masthead'>
         <Container text>
+        <Image centered src={favicon} size="mini" hidden/>
         <Header as='h1' inverted>ÌTHUÂN KHOKI</Header>
         <Header as='h6' inverted>用科技 sak 台灣母語</Header>
-        <br></br>
         </Container>
       </Segment>
     );
