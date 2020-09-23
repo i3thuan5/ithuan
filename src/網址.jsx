1fc384a1664da2c43b838f01fb11ebc0266f20f7
a8568730 <a8568730@hotmail.com>
2020-09-23 16:40:55 +0800

    * Bāng-chí piàⁿ-piàⁿ--leh.

--- src/網址.jsx
+++ src/網址.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import 網站 from './網站/網站';
-import 首頁 from './頁/首頁/首頁';
+import BangTsam from './網站/網站';
+import Tshu from './頁/首頁/首頁';
 import TsuanAn from './頁/TsuanAn';
 import SanPhin from './頁/SanPhin';
 import SuiSiann from './頁/SuiSiann';
-import 相關連結 from './頁/相關連結/相關連結';
-import 連絡交流 from './頁/連絡/連絡';
 
 import Debug from 'debug';
 Debug.enable('ithuan:*');
@@ -17,12 +15,12 @@ const root = document.getElementById('app');
 let history = browserHistory;
 render(
   <Router history={history}>
-          <Route path='/' component={網站}>
-              <IndexRoute component={首頁}/>
+          <Route path='/' component={BangTsam}>
+              <IndexRoute component={Tshu}/>
               <Route path='tsuanan' component={TsuanAn}/>
               <Route path='sanphin' component={SanPhin}/>
               <Route path='suisiann' component={SuiSiann}/>
-              <Route path='*' component={首頁}/>
+              <Route path='*' component={Tshu}/>
           </Route>
   </Router>,
   root
