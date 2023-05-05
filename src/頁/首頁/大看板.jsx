38ee4383226333e181e02fc0a7a996203f826c1c
Tira <hong@ithuan.tw>
2023-05-05 09:23:38 +0800

    加願景，較好demo

--- src/頁/首頁/大看板.jsx
+++ src/頁/首頁/大看板.jsx
@@ -9,11 +9,11 @@ export default class 大看板 extends Component {
 
   render() {
     return (
-      <Segment textAlign='center' inverted vertical className='masthead'>
+      <Segment textAlign='' inverted vertical className='masthead'>
         <Container text>
         <Image centered src={favicon} size="mini" hidden/>
-        <Header as='h1' inverted>ÌTHUÂN KHOKI</Header>
-        <Header as='h6' inverted>用科技 sak 台灣母語</Header>
+        <Header as='h1' inverted>意傳願景：用語言相交流溫暖</Header>
+        <p>Kā 20～50歲想欲感受語言溫暖情境 ê 人 kah 語言工作者 tàu-sann-kāng，hōo 人有法度去交流厝內、社會 bô-kâng 族群 ê 溫暖。</p>
         </Container>
       </Segment>
     );
