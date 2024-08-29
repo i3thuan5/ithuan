795a77787ab854e604802e62633b1d7156cb8d46
Tia i X570-ACE <hong@ithuan.tw>
2024-08-29 14:00:19 +0800

    Kaxabu Bizu用族語名。

--- src/頁/Kaxabu/index.jsx
+++ src/頁/Kaxabu/index.jsx
@@ -37,8 +37,8 @@ componentDidMount() {
       <Container text>
         <Segment basic size="large">
         <Item.Group relaxed>
-        <SingleItem title="噶哈巫語分類辭典（合作專案）" url="https://kaxabu.ithuan.tw/" meta="噶哈巫語分類辭典kaxabubizu"
-          >這个網站是電子版ê噶哈巫語分類辭典，網站是MIT開源授權。多謝潘永歷baba同意，規本辭典電子資料用CC BY-NC-SA公開出來。網站頂有完整辭典資料kah程式連結。</SingleItem>
+        <SingleItem title="Kaxabu Muwalak Misa A Ahan Bizu" url="https://kaxabu.ithuan.tw/" meta="噶哈巫語分類辭典"
+          >這个網站是電子版ê噶哈巫語分類辭典，網站是MIT開源授權。多謝潘永歷baba同意，規本辭典電子資料用CC BY-NC-SA公開出來。網站頂有完整辭典資料kah程式連結。（合作專案，意傳提供網站服務，2015-）</SingleItem>
         </Item.Group>
         </Segment>
       </Container>
