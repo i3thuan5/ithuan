47a649dfd55b290a119e01a566457dee5c8197d0
Tia i X570-ACE <hong@ithuan.tw>
2025-04-18 11:16:41 +0800

    補MisanoPangcah公開連結。

--- src/頁/Pangcah/index.jsx
+++ src/頁/Pangcah/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { Container, Header, List, Icon, Item, Image, Segment } from 'semantic-ui-react';
 import SingleItem from '../SingleItem'
 import LogoPangcah from '../../圖/pangcah.png'
@@ -40,7 +41,9 @@ componentDidMount() {
         <Segment basic size="large">
         <Item.Group relaxed>
           <SingleItem title=" MisanoPangcah/'Amis" logo={ LogoMisanoPangcah } url="https://apps.apple.com/tw/app/misanopangcah-amis/id6450070292" meta="阿美語口說練習"
-            >MisanoPangcah/‘Amis app是一個阿美語跟讀練習app，聽一遍例句發音後，再跟著唸一遍，語音辨識結果可以讓你馬上知道哪裡唸得與例句不同，並且為你的發音評分。（本計畫感謝文化部語言友善環境及創作應用與推廣補助）</SingleItem>
+            >MisanoPangcah/‘Amis app是一個阿美語跟讀練習app，聽一遍例句發音後，再跟著唸一遍，語音辨識結果可以讓你馬上知道哪裡唸得與例句不同，並且為你的發音評分。（本計畫感謝文化部語言友善環境及創作應用與推廣補助）
+            <Link to="/misanopangcah">
+          隱私權保護政策</Link></SingleItem>
           <SingleItem title="阿美語萌典" logo={LogoPangcah} url="https://amis.moedict.tw/#:co'ong" meta="阿美語線上辭典"
             >阿美語萌典是群眾協作掃描校對的數位化辭典專案，目前可以查到三部阿美語字典。（合作專案，發音功能由意傳語音技術支援）</SingleItem>
           <Item id='lamal'>
