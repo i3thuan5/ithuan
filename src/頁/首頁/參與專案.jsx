502724985377bae5d6ff797ab26db30cd3fb8747
Tia i X570-ACE <hong@ithuan.tw>
2025-04-25 17:26:28 +0800

    頭頁加厝鳥仔

--- src/頁/首頁/參與專案.jsx
+++ src/頁/首頁/參與專案.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Segment, Header, Grid, Image, Icon, List } from 'semantic-ui-react';
 import LogoTiA from '../../圖/line.png';
-import LogoSu5lui7 from '../../圖/su5lui7_hun1kip4.png';
+import LogoTshuTsiauA from '../../圖/tshutsiaua_tshing.png'
+import LogoStore from '../../圖/app-store-badge.png'
 import { Link } from 'react-router';
 
 class 參與專案 extends Component {
@@ -15,6 +16,26 @@ class 參與專案 extends Component {
       <Segment basic className="submasthead stripe slogan">
 
         <Grid container centered stackable verticalAlign='middle'>
+          <Grid.Column width={6}>
+            <Segment basic>
+              <Header>厝鳥仔講台語</Header>
+              <p>真人發音互動學習，開喙講台語無問題！</p>
+              <p>想講一口流利又親切的台語嗎？「厝鳥仔講台語」App 為你量身打造！這是一款專為台語學習者設計的口說練習應用程式，透過道地真人發音跟讀和即時語音回饋，幫助你掌握正確的台語發音，講出自然的語調，不再害怕開口說台語！</p>
+              <a href="https://apps.apple.com/tw/app/id6743581200">
+                <img src={LogoStore}
+                  alt="加入好友" height="36" border="0"/>
+              </a>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Segment basic><Image src={LogoTshuTsiauA} size='medium'/></Segment>
+          </Grid.Column>
+        </Grid>
+
+      </Segment>
+
+      <Segment basic className="stripe">
+          <Grid container centered stackable verticalAlign='middle'>
           <Grid.Column width={6}>
             <Segment basic>
               <Header>台語語音LineAI</Header>
@@ -34,21 +55,6 @@ class 參與專案 extends Component {
           <Grid.Column width={6}>
             <Segment basic><Image src={LogoTiA} size='medium'/></Segment>
           </Grid.Column>
-        </Grid>
-
-      </Segment>
-
-      <Segment basic className="stripe">
-          <Grid container centered stackable verticalAlign='middle'>
-            <Grid.Column width={6}>
-              <Segment basic>
-                <Header>客製化語料庫</Header>
-                <p>阮有濟款協助校對的工具，比如掠文本錯誤所在、顯示字詞收錄狀況、輸出詞頻統計報表。</p>
-              </Segment>
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <Segment basic><Image src={LogoSu5lui7} size='medium'/></Segment>
-            </Grid.Column>
           </Grid>
       </Segment>
     </div>
